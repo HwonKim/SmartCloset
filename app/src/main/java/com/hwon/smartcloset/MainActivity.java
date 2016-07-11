011d0eaf06f60135323110706499316afcbb16c0
hwon107 <hwkim107@naver.com>
2016-07-11 16:09:55 +0900

    Splash

--- app/src/main/java/com/hwon/smartcloset/MainActivity.java
+++ app/src/main/java/com/hwon/smartcloset/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hwon.smartcloset;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
---
+++ app/src/main/java/com/hwon/smartcloset/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.hwon.smartcloset;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Handler;
+
+/**
+ * Created by hwkim_000 on 2016-07-11.
+ */
+public class SplashActivity extends AppCompatActivity {
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        //3초 후 MainActivity 실행하고 SplashActivity 종료.
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplication(), MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
