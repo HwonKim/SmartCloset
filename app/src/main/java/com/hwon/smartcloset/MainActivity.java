4b9cfab36dcf17f990b2bf3b25e5b4485be256cf
hwon107 <hwkim107@naver.com>
2016-07-11 17:12:00 +0900

    Create Toolbar

--- app/src/main/java/com/hwon/smartcloset/MainActivity.java
+++ app/src/main/java/com/hwon/smartcloset/MainActivity.java
@@ -3,6 +3,8 @@ package com.hwon.smartcloset;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar Setting//
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        TextView title = (TextView)findViewById(R.id.toolbar_title);
+        setSupportActionBar(toolbar);
+
+        //기존 타이틀 없애고 새로운 타이틀 생성//
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        title.setText("Smart Closet");
+
     }
 }
